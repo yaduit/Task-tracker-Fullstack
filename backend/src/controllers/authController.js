5992661cf30ecdfa300a10d944a5f66f98093b33
yaduit <yk226411@gmail.com>
2026-03-07 12:03:27 +0530

    feat(tasks,auth): add validation, authorization and error handling to task controller,removed unwanted import in authcontroller

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,85 +1,79 @@
-import pool from "../config/db.js"
-import bcrypt from "bcrypt"
-import { generateToken } from "../utils/generateToken.js"
+import pool from "../config/db.js";
+import bcrypt from "bcrypt";
+import { generateToken } from "../utils/generateToken.js";
 
-export const registerUser = async(req,res)=>{
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
- try{
-
-  const {name,email,password} = req.body
-
-  if(!name || !email || !password){
-    return res.status(400).json({message:"All fields required"})
-  }
-
-  const normalizedEmail = email.toLowerCase()
-
-  const existing = await pool.query(
-    "SELECT id FROM users WHERE email=$1",
-    [normalizedEmail]
-  )
-
-  if(existing.rows.length > 0){
-    return res.status(409).json({message:"User already exists"})
-  }
-
-  const hashed = await bcrypt.hash(password,10)
-
-  const user = await pool.query(
-    "INSERT INTO users(name,email,password) VALUES($1,$2,$3) RETURNING id,name,email,role",
-    [name,normalizedEmail,hashed]
-  )
-
-  const token = generateToken(user.rows[0].id,user.rows[0].role)
-
-  res.cookie("token",token,{httpOnly:true})
-
-  res.status(201).json({
-    user:user.rows[0]
-  })
-
- }catch(err){
-  res.status(500).json({message:"Server error"})
- }
-}
-
-export const loginUser = async(req,res)=>{
-
- try{
-
-  const {email,password} = req.body
-
-  const user = await pool.query(
-    "SELECT * FROM users WHERE email=$1",
-    [email.toLowerCase()]
-  )
-
-  if(user.rows.length === 0){
-    return res.status(401).json({message:"Invalid credentials"})
-  }
-
-  const userData = user.rows[0]
-
-  const match = await bcrypt.compare(password,userData.password)
-
-  if(!match){
-    return res.status(401).json({message:"Invalid credentials"})
-  }
-
-  const token = generateToken(userData.id,userData.role)
-
-  res.cookie("token",token,{httpOnly:true})
-
-  res.json({
-    user:{
-      id:userData.id,
-      name:userData.name,
-      email:userData.email,
-      role:userData.role
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields required" });
     }
-  })
 
- }catch(err){
-  res.status(500).json({message:"Server error"})
- }
-}
+    const normalizedEmail = email.toLowerCase();
+
+    const existing = await pool.query("SELECT id FROM users WHERE email=$1", [
+      normalizedEmail,
+    ]);
+
+    if (existing.rows.length > 0) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    const user = await pool.query(
+      "INSERT INTO users(name,email,password) VALUES($1,$2,$3) RETURNING id,name,email,role",
+      [name, normalizedEmail, hashed],
+    );
+
+    const token = generateToken(user.rows[0].id, user.rows[0].role);
+
+    res.cookie("token", token, { httpOnly: true });
+
+    res.status(201).json({
+      user: user.rows[0],
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+    const user = await pool.query("SELECT * FROM users WHERE email=$1", [
+      email.toLowerCase(),
+    ]);
+
+    if (user.rows.length === 0) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const userData = user.rows[0];
+
+    const match = await bcrypt.compare(password, userData.password);
+
+    if (!match) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = generateToken(userData.id, userData.role);
+
+    res.cookie("token", token, { httpOnly: true });
+
+    res.json({
+      user: {
+        id: userData.id,
+        name: userData.name,
+        email: userData.email,
+        role: userData.role,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -1,57 +1,116 @@
-import pool from "../config/db.js"
+import pool from "../config/db.js";
 
-export const createTask = async(req,res)=>{
+const allowedStatus = ["pending", "in_progress", "completed"];
+export const createTask = async (req, res) => {
+  try {
+    const { title, description, status } = req.body;
+    if (!title) {
+      return res.status(400).json({ message: "Title is required" });
+    }
+    title = title.trim();
+    description = description?.trim();
 
- const {title,description,status} = req.body
+    if (status && !allowedStatus.includes(status)) {
+      return res.status(400).json({ message: "Invalid task status" });
+    }
+    const task = await pool.query(
+      "INSERT INTO tasks(title,description,status,user_id) VALUES($1,$2,$3,$4) RETURNING *",
+      [title, description || null, status || "pending", req.user.id],
+    );
 
- const task = await pool.query(
-   "INSERT INTO tasks(title,description,status,user_id) VALUES($1,$2,$3,$4) RETURNING *",
-   [title,description,status,req.user.id]
- )
+    return res.status(201).json(task.rows[0]);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
- res.status(201).json(task.rows[0])
-}
+export const getTasks = async (req, res) => {
+  try {
+    if (req.user.role === "admin") {
+      const tasks = await pool.query("SELECT * FROM tasks");
 
-export const getTasks = async(req,res)=>{
+      return res.json(tasks.rows);
+    }
 
- if(req.user.role === "admin"){
+    const tasks = await pool.query("SELECT * FROM tasks WHERE user_id=$1", [
+      req.user.id,
+    ]);
 
-  const tasks = await pool.query("SELECT * FROM tasks")
+    return res.json(tasks.rows);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
-  return res.json(tasks.rows)
+export const updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
 
- }
+    const { title, description, status } = req.body;
+    if (!title && !description && !status) {
+      return res.status(400).json({
+        message: "At least one field must be updated",
+      });
+    }
 
- const tasks = await pool.query(
-   "SELECT * FROM tasks WHERE user_id=$1",
-   [req.user.id]
- )
+    if (status && !allowedStatus.includes(status)) {
+      return res.status(400).json({ message: "Invalid task status" });
+    }
 
- res.json(tasks.rows)
-}
+    const existingTask = await pool.query("SELECT * FROM tasks WHERE id=$1", [
+      id,
+    ]);
 
-export const updateTask = async(req,res)=>{
+    if (existingTask.rows.length === 0) {
+      return res.status(404).json({ message: "Task not found" });
+    }
 
- const {id} = req.params
+    const task = existingTask.rows[0];
 
- const {title,description,status} = req.body
+    if (task.user_id !== req.user.id && req.user.role !== "admin") {
+      return res.status(403).json({ message: "Not authorized" });
+    }
 
- const task = await pool.query(
-   "UPDATE tasks SET title=$1,description=$2,status=$3 WHERE id=$4 RETURNING *",
-   [title,description,status,id]
- )
+    const updatedTask = await pool.query(
+      "UPDATE tasks SET title=$1, description=$2, status=$3 WHERE id=$4 RETURNING *",
+      [
+        title || task.title,
+        description || task.description,
+        status || task.status,
+        id,
+      ],
+    );
 
- res.json(task.rows[0])
-}
+    return res.json(updatedTask.rows[0]);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
-export const deleteTask = async(req,res)=>{
+export const deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
 
- const {id} = req.params
+    const existingTask = await pool.query("DELETE FROM tasks WHERE id=$1", [
+      id,
+    ]);
+    if (existingTask.rows.length === 0) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    const task = existingTask.rows[0];
 
- await pool.query(
-   "DELETE FROM tasks WHERE id=$1",
-   [id]
- )
+    if (task.user_id !== req.user.id && req.user.role !== "admin") {
+      return res.status(403).json({ message: "Not authorized" });
+    }
 
- res.json({message:"Task deleted"})
-}
+    await pool.query("DELETE FROM tasks WHERE id=$1", [id]);
+
+    return res.json({ message: "Task deleted" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
