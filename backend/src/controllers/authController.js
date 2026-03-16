95e723401d2027f60b32e6605e0eaeb5ebe3f314
yaduit <yk226411@gmail.com>
2026-03-16 21:24:49 +0530

    update: cookieoption samesite to None, removed unwanted theme file

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -6,7 +6,7 @@ import { generateToken } from "../utils/generateToken.js";
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "Strict",
+  sameSite: "None",
   maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
 };
 
